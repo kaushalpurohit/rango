2f7ef4a591e82ca293472487d0d9848a2093fd7a
Kaushal Purohit <kpurohit43@gmail.com>
2020-09-24 10:20:50 +0530

    Using telegram package to create bot

--- bot.py
+++ bot.py
@@ -9,17 +9,26 @@ import requests
 import re
 
 def bop(bot, update):
-    url = get_url()
     chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chat_id, photo=url)
+    bot.send_photo(chat_id=chat_id)
 
 def yts(bot, update):
-    search()
+    chat_id = update.message.chat_id
+    message = update.message.text
+    print(message,chat_id)
+
+def x(bot,update):
+    chat_id = update.message.chat_id
+    message = update.message.text
+    print(message,chat_id)
+
+
 def main():
     updater = Updater("1250079555:AAGxMQFXbCTR7hQCFcc7uLXzCYMyvEiTCU8")
     dp = updater.dispatcher
+    print(dp.chat_data)
     dp.add_handler(CommandHandler('yts',yts))
-    dp.add_handler(CommandHandler('1337x',1337x))
+    dp.add_handler(CommandHandler('1337x',x))
     updater.start_polling()
     updater.idle()
 
