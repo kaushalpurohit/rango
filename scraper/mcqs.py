a2dc6458c8972fb73a62dae53fab8d5b6bdfbde3
kaushalpurohit <kpurohit43@gmail.com>
2021-05-07 13:31:14 +0530

    add minor changes

--- scraper/mcqs.py
+++ scraper/mcqs.py
@@ -34,13 +34,13 @@ def generate_pdf(href):
     final_body = ""
     url = "https://www.sanfoundry.com" + href
     final_body = get_source(url, final_body)
-    f = open("files/test.html", "w")
+    f = open("test.html", "w")
     f.write(final_body)
     options = {
         "enable-local-file-access": None
     }
     try:
-        pdfkit.from_file('files/test.html', 'files/final.pdf', options=options)
+        pdfkit.from_file('test.html', 'final.pdf', options=options)
     except OSError as e:
         if 'Done' not in str(e):
             raise e
@@ -48,11 +48,11 @@ def generate_pdf(href):
         "Authorization": f"Bearer {FILEIO_KEY}"
     }
     data = {
-        "file": open("files/final.pdf", 'rb')
+        "file": open("final.pdf", 'rb')
     }
     upload_url = "https://file.io/"
     response = requests.post(upload_url, files=data, headers=headers).json()
-    remove("../files/final.pdf")
+    remove("final.pdf")
     return response["link"]
 
 
