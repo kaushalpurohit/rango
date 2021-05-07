22e80f669d6cf5635c8b1a0af1234803cb91af34
kaushalpurohit <kpurohit43@gmail.com>
2021-05-07 12:29:12 +0530

    Add file.io support

--- scraper/mcqs.py
+++ scraper/mcqs.py
@@ -2,8 +2,12 @@
 
 import requests
 import pdfkit
+from os import environ
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
+load_dotenv()
+FILEIO_KEY = environ.get("FILEIO_KEY")
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4)\
                 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 \
                 Safari/537.36"}
@@ -25,6 +29,28 @@ def get_source(url, final_body):
     return final_body
 
 
+def generate_pdf(url):
+    """Generate pdf file."""
+    final_body = ""
+    final_body = get_source(url, final_body)
+    f = open("../files/test.html", "w")
+    f.write(final_body)
+    options = {
+        'grayscale': 0,
+        'no background': 1
+    }
+    pdfkit.from_file('../files/test.html', '../files/final.pdf', options=options)
+    headers = {
+        "Authorization": f"Bearer {FILEIO_KEY}"
+    }
+    data = {
+        "file": open("/home/kaushal/yts-tele-bot/files/final.pdf", 'rb')
+    }
+    upload_url = "https://file.io/"
+    response = requests.post(upload_url, files=data, headers=headers).json()
+    return response["link"]
+
+
 if __name__ == '__main__':
     url = "https://www.sanfoundry.com/embedded-systems-questions-answers-processor-embedded-system/"
     final_body = ""
@@ -36,3 +62,12 @@ if __name__ == '__main__':
         'no background': 1
     }
     pdfkit.from_file('../files/test.html', '../files/final.pdf', options=options)
+    headers = {
+        "Authorization": f"Bearer {FILEIO_KEY}"
+    }
+    data = {
+        "file": open("/home/kaushal/yts-tele-bot/files/final.pdf", 'rb')
+    }
+    upload_url = "https://file.io/"
+    response = requests.post(upload_url, files=data, headers=headers).json()
+    print(response["link"])
